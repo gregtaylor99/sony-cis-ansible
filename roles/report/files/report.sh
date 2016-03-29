db54a2ed547acd4e195bb95a3fed5444d89e741d
Greg Taylor <gregtaylor999@gmail.com>
2016-03-29 16:45:21 -0400

    update report.sh

--- roles/report/files/report.sh
+++ roles/report/files/report.sh
@@ -3,8 +3,8 @@ set -x
 HOST=`hostname`
 echo $HOST
 
-cd /home/ec2-user
-unzip /tmp/ciscat-full-bundle-2016-01-06-v3.0.21.zip
+#cd /home/ec2-user
+#unzip /tmp/ciscat-full-bundle-2016-01-06-v3.0.21.zip
 chown -R ec2-user:ec2-user /home/ec2-user/*
  /home/ec2-user/cis-cat-full/CIS-CAT.sh  -f -props /home/ec2-user/cis-cat-full/misc/ciscat.properties -b /home/ec2-user/cis-cat-full/benchmarks/CIS_Amazon_Linux_2014.09-2015.03_Benchmark_v1.1.0-xccdf.xml
  mv /root/CIS-CAT_Results/*/*.html /home/ec2-user/report.html
